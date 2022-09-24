b9f96a30256c50c3dcf3cbbc20dc3e2b404487a0
tammekasra <54755302+tammekasra@users.noreply.github.com>
2022-09-24 15:14:11 +0300

    Testing
    Testing

--- main.py
+++ main.py
@@ -20,30 +20,60 @@ turn = 0
 while end_of_the_game:
     if turn == 0:
         move = int(input("Player 1, Make your Selection(0-6):"))
+
+        if move < 0 or move > 6: #If we select a wrong slot
+            print("Please select a proper slot!")
+            continue
+
+        if move in minmax.thisdict:  #This will check if the slot is full or not (max is 7)
+            if minmax.thisdict[move] == 6:
+                print("Please select another slot since this is FULL!")
+                continue
+        if move not in minmax.thisdict: #We will use a dict, since it is a lot easier and faster to do this way
+            minmax.thisdict[move] = 1
+        else:
+            minmax.thisdict[move] = minmax.thisdict[move] + 1
+
+
+        
         if Board.is_valid(board,move):
             row = Board.get_next_open_row(board,move)
             Board.move(board,row,move,1)
       #      Board.print_board(board)
-            if Winning_move.win(board, 1):
+            if Winning_move.win(board, 1): #We check if it is a winning game!
                         print("Player 1 wins!")
                         end_of_the_game = False
          
 
-    else:
-        predict = minmax.players_next_move(board)
-        if predict == -10:
-            move = random.randint(0,6)
-        else:
-            move = predict
+    else: #The A.I (sort of)
+
+       minimax_max = minmax.minimax(board)
+
+       break
+
+        
+
+       #predict = minmax.players_next_move(board)
+       # if predict == -10:
+       #     move = random.randint(0,6)
+       # else:
+       #     move = predict
+       # if move in minmax.thisdict:  #This will check if the slot is full or not (max is 7)
+       #     if minmax.thisdict[move] == 6:
+       #         continue
+       # if move not in minmax.thisdict: #We will use a dict, since it is a lot easier and faster to do this way
+       #     minmax.thisdict[move] = 1
+       # else:
+       #     minmax.thisdict[move] = minmax.thisdict[move] + 1
         
         #Player 2 will drop a piece on the board
-        if Board.is_valid(board,move):
-            row = Board.get_next_open_row(board,move)
-            Board.move(board,row,move,2)
-            Board.print_board(board)
-            if Winning_move.win(board, 2):
-                        print("Player 2 wins!")
-                        end_of_the_game = False
+       # if Board.is_valid(board,move):
+       #     row = Board.get_next_open_row(board,move)
+       #     Board.move(board,row,move,2)
+       #     Board.print_board(board)
+       #     if Winning_move.win(board, 2):
+       #                 print("Player 2 wins!")
+       #                 end_of_the_game = False
  
     Board.print_board(board) # We need to print it the matrix upside down
              
--- minmax.py
+++ minmax.py
@@ -9,17 +9,43 @@ x_axis = Board.x_axis
 y_axis = Board.y_axis
 
 
+thisdict = {}
+
+
 def players_next_move(board):
     value = -10
     print(y_axis)
     for i in range(y_axis):
         m = board.copy()
-        row = Board.get_next_open_row(m,i)
-        Board.move(m,row,i,1)
-        if Winning_move.win(m, 1):
+        if i in thisdict:
+            if thisdict[i] < 7:
+                row = Board.get_next_open_row(m,i)
+                Board.move(m,row,i,1)
+                if Winning_move.win(m, 1):
                     value = i
                     break
+        else:
+            continue
     return value
+
+
+def Generate_Children(board,turn):
+
+    list = []
+
+    for move in range(7):
+            board2 = board.copy()
+            row = Board.get_next_open_row(board2,move)
+            Board.move(board2,row,move,turn)
+            list.append(board2)
+            
+    return list
+            
+
+def minimax(board):
+    
+    for children in Generate_Children(board,2):
+        Board.print_board(children)
 
 
 
