76e0d79df2b54c1c2ffcec12859e1f6afe88e74b
tammekasra <54755302+tammekasra@users.noreply.github.com>
2022-10-08 15:09:03 +0300

    It.should.be.better.tried.to.implement.pygame.without.success

--- 4 in a row - MinMAX/main.py
+++ 4 in a row - MinMAX/main.py
@@ -10,23 +10,28 @@ import minmax
 import pygame
 import sys
 import math
-x_axis = Board.x_axis
-y_axis  = Board.y_axis
+import time
+x_axis = Board.x_axis # determine the length of the rows
+y_axis  = Board.y_axis # determine the length of the columns
  
 
 
 
 
+
 def main(board):     
-    
+    how_many_turns = 0
     Board.print_board(board)
     end_of_the_game = True
     turn = 0
+    Time_list = []
     another_algorithm_tofight_against_AI = int(input("Please select player(1) or a bot against the A.I (2)  "))
     while end_of_the_game: #If we reach to either a winning or a loosing position, we are going to put end_of_the_game as false
         if minmax.end_state(board):
             print("The game ended as a draw!") #If we reach to end of the game  - full of pieces, we get a draw!
-            return True
+            print(Time_list)
+            return (True,how_many_turns)
+        how_many_turns += 1
         if turn == 0:
             if another_algorithm_tofight_against_AI == 1:
                 move = int(input("Player 1, Make your Selection(0-6):  "))
@@ -52,9 +57,9 @@ def main(board):
             #      Board.print_board(board)
                     if Winning_move.win(board, 1): #We check if it is a winning game!
                                 print("Player 1 wins!")
-                                end_of_the_game = False
-                                minmax.thisdic = {} 
-                                return False   # We return false if and only if the player 1 wins (A.I has to either win or get a draw), we need to modify something if the test comes out as negative!
+                                end_of_the_game = False 
+                                print(Time_list)
+                                return (False,how_many_turns)   # We return false if and only if the player 1 wins (A.I has to either win or get a draw), we need to modify something if the test comes out as negative!
                                 break
                 else:
                     
@@ -93,7 +98,7 @@ def main(board):
                                 print("Player 1 wins!")
                                 end_of_the_game = False
                                 minmax.thisdict.clear()
-                                return False   # We return false if and only if the player 1 wins (A.I has to either win or get a draw), we need to modify something if the test comes out as negative!
+                                return (False,how_many_turns)   # We return false if and only if the player 1 wins (A.I has to either win or get a draw), we need to modify something if the test comes out as negative!
                                 break
                 
                 
@@ -107,8 +112,11 @@ def main(board):
             move = random.choice(moves)
             if Board.is_valid(board,move) == False:
                 continue
+            start = time.time()
             move2 = minmax.minimax(board, 5, -math.inf, math.inf, True,move) #Get the best move using MINIMAX algorithm!
-            print(move2) #- we can check the moves if we have bug....
+            end = time.time()
+           # print(move2) #- we can check the moves if we have bug....
+            Time_list.append([(how_many_turns/2),(move2),(end-start)])
             move = move2[0]
             row = Board.get_next_open_row(board,move)
             Board.move(board,row,move,2)
@@ -116,7 +124,8 @@ def main(board):
                 Board.print_board(board)
                 print("Player 2 wins!")
                 end_of_the_game = False
-                return True #we want our test to get a true answer!
+                print(Time_list)
+                return (True,how_many_turns) #we want our test to get a true answer!
                 
             Board.print_board(board)
             
