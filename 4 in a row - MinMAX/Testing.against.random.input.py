ded05237f7f796fcecdc483a571eab2f9fcdddfa
tammekasra <54755302+tammekasra@users.noreply.github.com>
2022-10-30 22:40:37 +0200

    last

--- 4 in a row - MinMAX/Testing.against.random.input.py
+++ 4 in a row - MinMAX/Testing.against.random.input.py
@@ -4,14 +4,19 @@ import unittest
 from unittest.mock import patch
 import main
 import random
-import Board
 import numpy as np
-list1 =  [1]*200 #This list is for the first testing code for the random input by the user (test_using_side_effect)
-list2 =  [2,5,5]*200 #This list is for the A.I (test_using_side_effect2)
+
+'''This select random moves '''
+list1 =  [1]*200 
+
+''' This picks to fight against the bad algorithm (the one who only blocks the winning moves)'''
+list2 =  [2,5,5]*200 
+
+''' This picks to fight himself! '''
 list3 = [3,5,5]*200
 
 
-
+''' We check how many turns it took to win one or either!'''
 how_many_turns_random = 0
 how_many_turns_bad_algorithm = 0
 how_many_turn_against_ai = 0
@@ -27,21 +32,31 @@ n_loops = 1 #How many times we are testing the same game over again!
 
 class Test(unittest.TestCase):
 
-    @patch('builtins.input', side_effect=(list1)) #This plays against random input from the user, so the A.I should win most of the time
+    '''This plays someone who plays completely randomly! '''
+
+    @patch('builtins.input', side_effect=(list1)) 
     def test_using_side_effect(self, mock_input):
         global n_loops
+
+        ''' We '''
         for p in range(n_loops): 
             board1 = np.zeros((6,7)) 
             for i in range(200):
                 if i > 0:
                     list1[i] = random.choice(['0','1', '2', '3', '4', '5', '6'])
+            '''We go through the minmax using random varaibles '''
+
             a = main.main(board1)
+
+            '''How many turns it took! '''
             calling_1 = a[0]
             global how_many_turns_random 
             how_many_turns_random += a[1]
+
+            ''' To see if it comes back as either won or drawn!'''
             self.assertTrue(calling_1 == True)
     
-    @patch('builtins.input', side_effect=(list3)) #This plays against A.I, an algorithm that does not try to win, but block every 4th winning slot for the A.I
+    @patch('builtins.input', side_effect=(list3)) 
     def test_using_side_effect2(self, mock_input):
         global n_loops
         for p in range(n_loops): 
@@ -49,13 +64,17 @@ class Test(unittest.TestCase):
             for i in range(100):
                 if i > 2:
                     list2[i] = random.choice(['0','1', '2', '3', '4', '5', '6'])
+
+            ''' Number of moves against the bad algorithm!! '''
             a2 = main.main(board2)
             calling_2 = a2[0] 
             global how_many_turns_bad_algorithm
             how_many_turns_bad_algorithm += a2[1]
-            #calling_2 = main.main(position1)
+
+
+            ''' To see if it comes back as either won or drawn!'''
             self.assertTrue(calling_2 == True)
-    @patch('builtins.input', side_effect=(list2)) #This plays against A.I himself, but forsome reason it never ends up in a draw...
+    @patch('builtins.input', side_effect=(list2)) 
     def test_using_side_effect3(self, mock_input):
         global n_loops
         for p in range(n_loops): 
@@ -68,7 +87,7 @@ class Test(unittest.TestCase):
             global how_many_turn_against_ai
             how_many_turn_against_ai += a3[1]
             
-            #calling_2 = main.main(position1)
+            ''' To see if it comes back as either won or drawn!'''
             self.assertTrue(calling_3 == True)
         print("Against random inputs, takes turns on average = ", how_many_turns_random/n_loops)
         print("Against the bad algorithm, takes turns on average = ",how_many_turns_bad_algorithm/n_loops )
